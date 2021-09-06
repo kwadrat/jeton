17d75b7505c5f79db20a2b036856659ece56fdea
Piotr Kasprzyk <ciri@ciri.pl>
2021-09-06 13:07:41 +0200

    Adjust indent

--- src/fmd5.cpp
+++ src/fmd5.cpp
@@ -10,28 +10,28 @@
 
 md5_Class::md5_Class(void)
 {
- RodzajWej = 'M';
- RodzajWyj = 'M';
- BufSize = 0;
- BufPtr = NULL;
+    RodzajWej = 'M';
+    RodzajWyj = 'M';
+    BufSize = 0;
+    BufPtr = NULL;
 }
 
 /******************************************************************************/
 
 void md5_Class::LocalDestructor(void)
 {
- if(NextProcess != NULL)
- {
-  NextProcess->LocalDestructor();
-  delete NextProcess;
-  NextProcess = NULL;
- }
- if(BufPtr != NULL)
- {
-  free(BufPtr);
-  BufPtr = NULL;
-  BufSize = 0;
- }
+    if(NextProcess != NULL)
+    {
+        NextProcess->LocalDestructor();
+        delete NextProcess;
+        NextProcess = NULL;
+    }
+    if(BufPtr != NULL)
+    {
+        free(BufPtr);
+        BufPtr = NULL;
+        BufSize = 0;
+    }
 }
 
 /******************************************************************************/
@@ -39,62 +39,62 @@ void md5_Class::LocalDestructor(void)
 /* Jedynym parametrem jest liczba bajtów w buforze */
 int md5_Class::Init(int argc, char *argv[])
 {
- int ile;
- int status;
+    int ile;
+    int status;
 
- status = RESULT_OFF;
- return status;
+    status = RESULT_OFF;
+    return status;
 }
 
 /******************************************************************************/
 
 int md5_Class::Work(int kmn, Byte * poczatek, int ile)
 {
- int status;
+    int status;
 
- status = RESULT_OFF;
- if(State != STATE_OFF)
- {
-  switch(kmn)
-  {
-   case SAND_GENERAL:
-   {
-    status = Obsluga_SAND_GENERAL(kmn, poczatek, ile);
-    break;
-   }
-   case SAND_OFF:
-   {
-    status = Obsluga_SAND_OFF(kmn, poczatek, ile);
-    break;
-   }
-   default:
-   {
-    SygErrorParm("Nieznany typ komunikatu %d", kmn);
-    ZmienStan(STATE_OFF, KIER_INNY);
-    break;
-   }
-  }
- }
- return status;
+    status = RESULT_OFF;
+    if(State != STATE_OFF)
+    {
+        switch(kmn)
+        {
+            case SAND_GENERAL:
+            {
+                status = Obsluga_SAND_GENERAL(kmn, poczatek, ile);
+                break;
+            }
+            case SAND_OFF:
+            {
+                status = Obsluga_SAND_OFF(kmn, poczatek, ile);
+                break;
+            }
+            default:
+            {
+                SygErrorParm("Nieznany typ komunikatu %d", kmn);
+                ZmienStan(STATE_OFF, KIER_INNY);
+                break;
+            }
+        }
+    }
+    return status;
 }
 
 /******************************************************************************/
 
 int md5_Class::Obsluga_SAND_GENERAL(int kmn, Byte * poczatek, int ile)
 {
- int status;
+    int status;
 
- status = RESULT_OFF;
- return status;
+    status = RESULT_OFF;
+    return status;
 }
 
 /******************************************************************************/
 
 int md5_Class::Obsluga_SAND_OFF(int kmn, Byte * poczatek, int ile)
 {
- assert(ile == KIER_PROSTO);
- ZmienStan(STATE_OFF, ile);
- return RESULT_OFF;
+    assert(ile == KIER_PROSTO);
+    ZmienStan(STATE_OFF, ile);
+    return RESULT_OFF;
 }
 
 /******************************************************************************/
