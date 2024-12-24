974fc91619ac8168c92fead2d47256985a58b5c7
Piotr Kasprzyk <ciri@ciri.pl>
2024-12-24 08:08:57 +0100

    Fix: Carlisle

--- src/jeton.cpp
+++ src/jeton.cpp
@@ -46,7 +46,7 @@ int main(int argc, char * argv[])
         {
             if(CreateChainLogic(argc - 1, argv + 1))
             {
-                /* Circle in the sand (Belinda Carlile?) - uruchom łańcuch procesów */
+                /* Circle in the sand (Belinda Carlisle) - uruchom łańcuch procesów */
                 TopMember.RunProcesses();
             }
             else
